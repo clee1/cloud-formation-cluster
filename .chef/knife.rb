66331f6a347cc39e9d7eaf13ad8f3cbb6536f2e6
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2016-10-12 05:21:03 +0300

    set vim as default editor

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,5 @@ node_name                "eugenzyx"
 client_key               "#{current_dir}/eugenzyx.pem"
 chef_server_url          "https://ec2-54-175-81-229.compute-1.amazonaws.com/organizations/home"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+
+knife[:editor] = "vim"
