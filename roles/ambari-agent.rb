a501ead90630b7e64af23b9cfd490289da116a99
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2016-10-20 21:00:51 +0300

    selinux::enabled -> selinux::enforcing

--- roles/ambari-agent.rb
+++ roles/ambari-agent.rb
@@ -3,7 +3,7 @@ description "A role for the ambari server."
 run_list "recipe[selinux::disabled]",
          "recipe[common::default]",
          "recipe[ambari-agent::default]",
-         "recipe[selinux::enabled]"
+         "recipe[selinux::enforcing]"
 override_attributes({
   "starter_name" => "Eugen Prokhorenko",
 })
--- roles/ambari-server.rb
+++ roles/ambari-server.rb
@@ -4,7 +4,7 @@ run_list "recipe[postgresql::client]",
          "recipe[selinux::disabled]",
          "recipe[common::default]",
          "recipe[ambari-server::default]",
-         "recipe[selinux::enabled]"
+         "recipe[selinux::enforcing]"
 override_attributes({
   "starter_name" => "Eugen Prokhorenko",
 })
