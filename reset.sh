a14ed5a8a0a6d902cce0ee4285b70656c224421a
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2016-10-30 21:04:22 +0200

    reset tunnel.sh on execution of reset.sh

--- reset.sh
+++ reset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mv -v .chef/knife.rb.old .chef/knife.rb
+mv -v tunnel.sh.old tunnel.sh
 git checkout data_bags/nodes/ambari-server.json
 git checkout data_bags/nodes/ambari-agents.json
 rm -v data_bags/nodes/ambari-server.json.old
